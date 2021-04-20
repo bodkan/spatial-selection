56ac66a59659243e2d916ce14a52c73c94d81a45
Martin Petr <git@bodkan.net>
2021-04-20 16:43:30 +0200

    Update the setup script

--- 00_setup.R
+++ 00_setup.R
@@ -1,3 +1,6 @@
+install.packages(c("devtools", "sf", "tidyverse"))
+devtools::install_github("bodkan/spammr", ref = "2a652c6")
+
 # locations of generated SLiM scripts and output files
 dir.create("slim/")
 dir.create("results/")

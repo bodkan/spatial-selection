2a82162ab8f70d76f6fe7f1075a01d6a47976bb3
Martin Petr <git@bodkan.net>
2021-04-22 10:30:46 +0200

    Update to the latest spammr version

--- 00_setup.R
+++ 00_setup.R
@@ -1,5 +1,5 @@
 install.packages(c("devtools", "sf", "tidyverse"))
-devtools::install_github("bodkan/spammr", ref = "0f4b653")
+devtools::install_github("bodkan/spammr", ref = "628a010")
 
 # locations of generated SLiM scripts and output files
 dir.create("slim/")
